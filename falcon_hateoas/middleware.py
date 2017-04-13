5dffaa5a3522cc6d07845cb5b34372eae9dcc05c
Michal Juranyi <michal.juranyi@vnet.eu>
2017-04-13 14:18:18 +0200

    Revert faulty improvement
    Signed-off-by: Michal Juranyi <michal.juranyi@vnet.eu>

--- falcon_hateoas/middleware.py
+++ falcon_hateoas/middleware.py
@@ -15,15 +15,16 @@ class AlchemyJSONEncoder(json.JSONEncoder):
     def default(self, o):
         if self._is_alchemy_object(o):
             d = {}
-            for col_name, col in o.__table__.columns.items():
-                if hasattr(col, 'isoformat'):
-                    d[col_name] = col.isoformat()
-                elif isinstance(col, datetime.timedelta):
-                    d[col_name] = str(col)
-                elif isinstance(col, decimal.Decimal):
-                    d[col_name] = float(col)
+            for col in o.__table__.columns.keys():
+                value = getattr(o, col)
+                if hasattr(value, 'isoformat'):
+                    d[col] = value.isoformat()
+                elif isinstance(value, datetime.timedelta):
+                    d[col] = str(value)
+                elif isinstance(value, decimal.Decimal):
+                    d[col] = float(value)
                 else:
-                    d[col_name] = col
+                    d[col] = value
             return d
         else:
             return super(AlchemyJSONEncoder, self).default(o)
