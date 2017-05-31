791f64250d5e7c2ac2c5e01aa1e890dbefbc0417
Michal Juranyi <michal.juranyi@vnet.eu>
2017-05-31 12:25:20 +0200

    Remove dead code from AlchemyJSONEncoder
    Signed-off-by: Michal Juranyi <michal.juranyi@vnet.eu>

--- falcon_hateoas/middleware.py
+++ falcon_hateoas/middleware.py
@@ -1,33 +1,22 @@
 import json
 import decimal
 import datetime
-import sqlalchemy
 
 
 class AlchemyJSONEncoder(json.JSONEncoder):
-    def _is_alchemy_object(self, obj):
-        try:
-            sqlalchemy.orm.base.object_mapper(obj)
-            return True
-        except sqlalchemy.orm.exc.UnmappedInstanceError:
-            return False
-
     def default(self, o):
-        if self._is_alchemy_object(o):
-            d = {}
-            for col in o.__table__.columns.keys():
-                value = getattr(o, col)
-                if hasattr(value, 'isoformat'):
-                    d[col] = value.isoformat()
-                elif isinstance(value, datetime.timedelta):
-                    d[col] = str(value)
-                elif isinstance(value, decimal.Decimal):
-                    d[col] = float(value)
-                else:
-                    d[col] = value
-            return d
-        else:
-            return super(AlchemyJSONEncoder, self).default(o)
+        d = {}
+        for col in o.__table__.columns.keys():
+            value = getattr(o, col)
+            if hasattr(value, 'isoformat'):
+                d[col] = value.isoformat()
+            elif isinstance(value, datetime.timedelta):
+                d[col] = str(value)
+            elif isinstance(value, decimal.Decimal):
+                d[col] = float(value)
+            else:
+                d[col] = value
+        return d
 
 
 class JsonMiddleware:
